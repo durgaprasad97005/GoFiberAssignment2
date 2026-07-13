f457e49f7a2929bc5ce0dddaec2e5094210e2fde
Durga Prasad <durgaprasad97005@gmail.com>
2026-07-13 12:06:30 +0530

    Updated auth.go file

--- src/controllers/auth.go
+++ src/controllers/auth.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/durgaprasad97005/GoFiberAssignment2/src/db"
 	"github.com/durgaprasad97005/GoFiberAssignment2/src/models"
+	"github.com/durgaprasad97005/GoFiberAssignment2/src/utils"
 	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v3"
 	"github.com/golang-jwt/jwt/v5"
@@ -31,31 +32,34 @@ func SignUp(c fiber.Ctx) error {
 	// Parsing request body
 	var body models.User
 	if err := c.Bind().Body(&body); err != nil {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"success": false,
-			"message": "Error parsing body",
-			"error":   err.Error(),
-		})
+		return utils.Error(
+			c, 
+			fiber.StatusBadRequest, 
+			"Error parsing body", 
+			err.Error(), 
+		)
 	}
 
 	// Validate the request body
 	validate := validator.New()
 	if err := validate.Struct(&body); err != nil {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"success": false,
-			"message": "Data validation error",
-			"error":   err.Error(),
-		})
+		return utils.Error(
+			c, 
+			fiber.StatusBadRequest, 
+			"Data validation error", 
+			err.Error(), 
+		)
 	}
 
 	// Get the collection
 	collection := db.GetCollection("users")
 	if collection == nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"success": false,
-			"message": "Unable to find collection",
-			"error":   "Internal server error",
-		})
+		return utils.Error(
+			c, 
+			fiber.StatusInternalServerError, 
+			"Unable to find collection", 
+			"Internal server error", 
+		)
 	}
 
 	// Context creation
@@ -66,21 +70,23 @@ func SignUp(c fiber.Ctx) error {
 	var existingUser models.User
 	err := collection.FindOne(ctx, bson.M{"email": body.Email}).Decode(&existingUser)
 	if err == nil {
-		return c.Status(fiber.StatusConflict).JSON(fiber.Map{
-			"success": false,
-			"message": "There exists another user with the given email",
-			"error":   "User creation failed",
-		})
+		return utils.Error(
+			c, 
+			fiber.StatusConflict, 
+			"There exists another user with the given email", 
+			"User creation failed", 
+		)
 	}
 
 	// Create password hash
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(body.Password), 12)
 	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"success": false,
-			"message": "Error while hashing password",
-			"error":   err.Error(),
-		})
+		return utils.Error(
+			c, 
+			fiber.StatusInternalServerError, 
+			"Error while hashing password", 
+			err.Error(), 
+		)
 	}
 
 	body.PasswordHash = string(hashedPassword)
@@ -97,11 +103,12 @@ func SignUp(c fiber.Ctx) error {
 	// Insert user
 	result, err := collection.InsertOne(ctx, userDto)
 	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"success": false,
-			"message": "Error while trying to signup",
-			"error":   err.Error(),
-		})
+		return utils.Error(
+			c, 
+			fiber.StatusInternalServerError, 
+			"Error while trying to signup", 
+			err.Error(), 
+		)
 	}
 
 	// Updating fields for response
@@ -133,30 +140,33 @@ func Login(c fiber.Ctx) error {
 	// Parse body of the reques
 	var body models.User
 	if err := c.Bind().Body(&body); err != nil {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"success": false, 
-			"message": "Error while parsing body", 
-			"error": err.Error(), 
-		})
+		return utils.Error(
+			c, 
+			fiber.StatusBadRequest, 
+			"Error while parsing body", 
+			err.Error(), 
+		)
 	}
 
 	// Check whether required data exists or not
 	if body.Email == "" || body.Password == "" {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"success": false, 
-			"message": "Email and Password fields are required", 
-			"error": "Required fields are missing", 
-		})
+		return utils.Error(
+			c, 
+			fiber.StatusBadRequest, 
+			"Email and Password fields are required", 
+			"Required fields are missing", 
+		)
 	}
 
 	// Get collection
 	collection := db.GetCollection("users")
 	if collection == nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"success": false, 
-			"message": "Unable to get the collection", 
-			"error": "Internal server error", 
-		})
+		return utils.Error(
+			c, 
+			fiber.StatusInternalServerError, 
+			"Unabe to get the collection", 
+			"Internal server error", 
+		)
 	}
 
 	// Create context variable
@@ -166,30 +176,33 @@ func Login(c fiber.Ctx) error {
 	// Check whether user record exists
 	var existingUser models.User
 	if err := collection.FindOne(ctx, bson.M{"email": body.Email}).Decode(&existingUser); err != nil {
-		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
-			"success": false, 
-			"message": "There exists no user with the given email", 
-			"error": err.Error(), 
-		})
+		return utils.Error(
+			c, 
+			fiber.StatusNotFound, 
+			"There exists no user with the given email", 
+			err.Error(), 
+		)
 	}
 
 	// Compare password hash
 	if err := bcrypt.CompareHashAndPassword([]byte(existingUser.PasswordHash), []byte(body.Password)); err != nil {
-		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
-			"success": false, 
-			"message": "Wrong password", 
-			"error": err.Error(), 
-		})
+		return utils.Error(
+			c, 
+			fiber.StatusUnauthorized, 
+			"Wrong password", 
+			err.Error(), 
+		)
 	}
 
 	// Generate jwt token string
 	jwtTokenString, err := getJwtTokenString(existingUser)
 	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"success": false, 
-			"message": "Unable to generate token string", 
-			"error": err.Error(), 
-		})
+		return utils.Error(
+			c, 
+			fiber.StatusInternalServerError, 
+			"Unable to generate token string", 
+			err.Error(), 
+		)
 	}
 
 	// Success response
